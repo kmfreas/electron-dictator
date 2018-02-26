52563bc984b1f56acbecfc87d1c762c5250efde9
krez <kmfreas@gmail.com>
2018-02-26 07:01:51 -0500

    add font awesome

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue';
 import axios from 'axios';
+import fontawesome from '@fortawesome/fontawesome';
+import { faPlay, faPause } from '@fortawesome/fontawesome-free-solid';
 
 import App from './App';
 import router from './router';
 import store from './store';
+
+fontawesome.library.add(faPlay, faPause);
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
 Vue.http = Vue.prototype.$http = axios;
