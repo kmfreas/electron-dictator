de3517ceb996575d56e653e22faa212b778bb2e1
krez <kmfreas@gmail.com>
2018-02-26 13:09:24 -0500

    add navbar

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,15 +1,36 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+      <div class="navbar-menu is-active">
+        <div class="navbar-start">
+          <div class="navbar-item">
+            <router-link to="/">Dictator</router-link>
+          </div>
+        </div>
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <router-link to="/recordings">Your Recordings</router-link>
+          </div>
+          <div class="navbar-item">
+            <router-link to="/settings">Settings</router-link>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <div class="section">
+      <div class="container">
+        <router-view></router-view>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'electron-dictator',
-  };
+export default {
+  name: 'electron-dictator',
+};
 </script>
 
 <style>
-  /* CSS */
+/* CSS */
 </style>
