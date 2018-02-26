ba63109bc02e7786f5aa62cb635f3a39ed137f3c
krez <kmfreas@gmail.com>
2018-02-26 08:59:01 -0500

    add stream component, change layout

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,18 +1,30 @@
 <template>
-  <div>
-    <record-control></record-control>
-    <recordings></recordings>
+  <div class="section">
+    <div class="container">
+      <div class="columns">
+        <div class="column">
+          <record-control></record-control>
+          <recordings></recordings>
+        </div>
+        <div class="column">
+          
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import RecordControl from './RecordControl';
 import Recordings from './Recordings';
+import Stream from './Stream';
+
 export default {
   name: 'landing-page',
   components: {
     RecordControl,
     Recordings,
+    Stream,
   },
   methods: {
   },
