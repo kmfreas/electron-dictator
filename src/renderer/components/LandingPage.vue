9969e55d1a88d3379563c5c06bd94eb22ce16ad9
krez <kmfreas@gmail.com>
2018-02-26 13:10:16 -0500

    layout changes

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,33 +1,25 @@
 <template>
-  <div class="section">
-    <div class="container">
-      <div class="columns">
-        <div class="column">
-          <record-control></record-control>
-          <recordings></recordings>
-        </div>
-        <div class="column">
-          
-        </div>
-      </div>
+  <div class="columns">
+    <div class="column">
+      <record-control></record-control>
+    </div>
+    <div class="column">
+      <stream></stream>
     </div>
   </div>
 </template>
 
 <script>
 import RecordControl from './RecordControl';
-import Recordings from './Recordings';
 import Stream from './Stream';
 
 export default {
   name: 'landing-page',
   components: {
     RecordControl,
-    Recordings,
     Stream,
   },
-  methods: {
-  },
+  methods: {},
 };
 </script>
 <style lang="scss">
