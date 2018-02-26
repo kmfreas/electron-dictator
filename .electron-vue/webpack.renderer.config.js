a9d250a78d35ae489b8aed372bf11453364d1e57
krez <kmfreas@gmail.com>
2018-02-26 06:40:09 -0500

    setup styles with bulma and sass-loader

--- .electron-vue/webpack.renderer.config.js
+++ .electron-vue/webpack.renderer.config.js
@@ -68,8 +68,8 @@ let rendererConfig = {
           options: {
             extractCSS: process.env.NODE_ENV === 'production',
             loaders: {
-              sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax=1',
-              scss: 'vue-style-loader!css-loader!sass-loader'
+              sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax=1&data=@import "./src/renderer/styles/_variables.scss"; @import "./node_modules/bulma/bulma.sass"; @import "./src/renderer/styles/main.scss";',
+              scss: 'vue-style-loader!css-loader!sass-loader?data=@import "./src/renderer/styles/_variables.scss"; @import "./node_modules/bulma/bulma.sass"; @import "./src/renderer/styles/main.scss";'
             }
           }
         }
