327d8939d6a37e3ebeb8106ec229bb3d6e174397
krez <kmfreas@gmail.com>
2018-02-26 20:09:05 -0500

    store google speech api credentials instead of api key

--- src/renderer/components/Settings.vue
+++ src/renderer/components/Settings.vue
@@ -2,35 +2,69 @@
   <div>
     <div class="field">
       <div class="control">
-        <input class="input" type="text" v-model="key" placeholder="Google Speech API Key">
+        <input class="input" type="text" v-model="projectId" placeholder="Google Speech Project ID">
       </div>
+    </div>
+    <div class="file has-name is-fullwidth field">
+      <label class="file-label">
+        <input class="file-input" type="file" name="credentials" @change="onFileChange">
+        <span class="file-cta">
+          <span class="file-icon">
+            <font-awesome-icon icon="upload" />
+          </span>
+          <span class="file-label">
+            Choose a file…
+          </span>
+        </span>
+        <span class="file-name">
+          <span v-if="file && file.name">{{file.name}}</span>
+        </span>
+      </label>
     </div>
     <div class="field">
       <div class="control">
-        <button class="button is-link" @click="saveKey">Save</button>
+        <button class="button is-link" @click="save">Save</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import path from 'path';
+import FontAwesomeIcon from '@fortawesome/vue-fontawesome';
+// eslint-disable-next-line
+import { remote } from 'electron';
 import Database from '../api/database';
+const fs = require('fs');
 
 export default {
+  components: {
+    FontAwesomeIcon,
+  },
   data() {
     return {
-      key: null,
+      projectId: null,
+      file: null,
     };
   },
   mounted() {
-    Database.options.get('google_api_key').then((key) => {
-      console.log(key);
-      this.key = key;
+    Database.options.get('speech_project_id').then((projectId) => {
+      this.projectId = projectId;
     });
   },
   methods: {
-    saveKey() {
-      Database.options.update('google_api_key', this.key);
+    onFileChange(e) {
+      const file = e.target.files || e.dataTransfer.files;
+      if (!file.length) {
+        return;
+      }
+      this.file = file[0];
+    },
+    save() {
+      Database.options.update('speech_project_id', this.projectId);
+      if (this.file && this.file.path) {
+        fs.createReadStream(this.file.path).pipe(fs.createWriteStream(path.join(remote.app.getPath('userData'), '/credentials.json')));
+      }
     },
   },
 };
