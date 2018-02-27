9b0154e4dd5a032d6d36e56dca9d3cc3efce4f11
krez <kmfreas@gmail.com>
2018-02-27 15:26:13 -0500

    run yarn and autoformatting

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -94,14 +94,14 @@
       fixed: false,
       items: [
         { icon: 'apps', title: 'Welcome', to: '/' },
-        { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
+        { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' },
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
-    })
-  }
+      title: 'Vuetify.js',
+    }),
+  };
 </script>
 
 <style>
