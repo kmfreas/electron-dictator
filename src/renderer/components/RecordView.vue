f76411b8dd0d2f5c9ffe534045b605d34f101335
krez <kmfreas@gmail.com>
2018-03-05 19:10:15 -0500

    layout changes

--- src/renderer/components/RecordView.vue
+++ src/renderer/components/RecordView.vue
@@ -1,10 +1,12 @@
 <template>
-  <v-layout row wrap>
-    <v-flex md8>
-      <record-control></record-control>
+  <v-layout column justify-center>
+    <v-flex style="height: 100%">
+      <div class="main">
+        <record-list></record-list>
+      </div>
     </v-flex>
-    <v-flex xs12 class="mt-5">
-      <record-list></record-list>
+    <v-flex class="sub-footer">
+      <record-control></record-control>
     </v-flex>
   </v-layout>
 </template>
@@ -20,3 +22,23 @@ export default {
   },
 };
 </script>
+
+<style>
+.sub-footer {
+  align-self: flex-end;
+  width: 100%;
+  flex: 0;
+}
+
+.main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+.main .card {
+  width: 100%;    
+}
+</style>
--- src/renderer/components/SettingsView.vue
+++ src/renderer/components/SettingsView.vue
@@ -1,42 +1,44 @@
 <template>
-  <v-card>
-    <v-toolbar color="cyan" dark>
-      <v-toolbar-title>Setup</v-toolbar-title>
-    </v-toolbar>
-    <v-card-text v-show="!show">
-      Your credentials are correctly setup.
-    </v-card-text>
-    <v-slide-y-transition>
-      <v-card-text v-show="show">
-        <p class="headline">Instructions</p>
-        <p class="mb-0">
-          Create a project on Google Cloud Console and enable the speech API. <v-icon style="cursor:pointer; vertical-align: text-top" class="body-1" color="primary" @click="open('https://cloud.google.com/speech/docs/quickstart')">open_in_new</v-icon>
-        </p>
-        <p>
-          Create a service account key and add the key below. <v-icon style="cursor:pointer; vertical-align: text-top" class="body-1" color="primary" @click="open('https://console.cloud.google.com/apis/credentials/serviceaccountkey')">open_in_new</v-icon>
-        </p>
-        <p class="headline mb-0">Add Credentials</p>
-        <v-form v-model="valid" ref="form" lazy-validation>
-          <file-input @fileAdded="handleFile" v-model="fileName" label="Google Speech Service Account Key File" :required="true" :rules="fileNameRules" accept=".json"></file-input>
-        </v-form>
+  <v-layout column>
+    <v-card>
+      <v-toolbar color="cyan" dark>
+        <v-toolbar-title>Setup</v-toolbar-title>
+      </v-toolbar>
+      <v-card-text v-show="!show">
+        Your credentials are correctly setup.
       </v-card-text>
-    </v-slide-y-transition>
-    <v-card-actions>
-      <v-btn dark @click="saveSettings" color="primary" v-if="show">
-        Save
-      </v-btn>
-      <v-btn @click="show = !show" color="primary" v-if="!show">
-        Edit
-      </v-btn>
-      <v-btn v-if="show && credentialsValid" @click="show = !show">
-        Cancel
-      </v-btn>
-    </v-card-actions>
-    <v-snackbar :timeout="6000" :bottom="true" v-model="showSnackbar">
-      Settings saved
-      <v-btn flat color="primary" @click.native="showSnackbar = false">Close</v-btn>
-    </v-snackbar>
-  </v-card>
+      <v-slide-y-transition>
+        <v-card-text v-show="show">
+          <p class="headline">Instructions</p>
+          <p class="mb-0">
+            Create a project on Google Cloud Console and enable the speech API. <v-icon style="cursor:pointer; vertical-align: text-top" class="body-1" color="primary" @click="open('https://cloud.google.com/speech/docs/quickstart')">open_in_new</v-icon>
+          </p>
+          <p>
+            Create a service account key and add the key below. <v-icon style="cursor:pointer; vertical-align: text-top" class="body-1" color="primary" @click="open('https://console.cloud.google.com/apis/credentials/serviceaccountkey')">open_in_new</v-icon>
+          </p>
+          <p class="headline mb-0">Add Credentials</p>
+          <v-form v-model="valid" ref="form" lazy-validation>
+            <file-input @fileAdded="handleFile" v-model="fileName" label="Google Speech Service Account Key File" :required="true" :rules="fileNameRules" accept=".json"></file-input>
+          </v-form>
+        </v-card-text>
+      </v-slide-y-transition>
+      <v-card-actions>
+        <v-btn dark @click="saveSettings" color="primary" v-if="show">
+          Save
+        </v-btn>
+        <v-btn @click="show = !show" color="primary" v-if="!show">
+          Edit
+        </v-btn>
+        <v-btn v-if="show && credentialsValid" @click="show = !show">
+          Cancel
+        </v-btn>
+      </v-card-actions>
+      <v-snackbar :timeout="6000" :bottom="true" v-model="showSnackbar">
+        Settings saved
+        <v-btn flat color="primary" @click.native="showSnackbar = false">Close</v-btn>
+      </v-snackbar>
+    </v-card>
+  </v-layout>
 </template>
 
 <script>
