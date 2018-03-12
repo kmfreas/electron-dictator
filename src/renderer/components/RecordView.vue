25800ce8ecae31ab0c1434cdfb506e87d15a5631
krez <kmfreas@gmail.com>
2018-03-12 09:40:29 -0400

    add recordmessages if credentials are needed

--- src/renderer/components/RecordView.vue
+++ src/renderer/components/RecordView.vue
@@ -1,10 +1,11 @@
 <template>
-  <v-layout column justify-center fill-height>
-    <v-flex>
+  <v-layout column align-content-start justify-start>
+    <record-messages v-if="!credentials.valid"></record-messages>
+    <v-flex class="shrink" v-if="credentials.valid">
       <record-control></record-control>
     </v-flex>
     <record-notifications></record-notifications>
-    <v-flex fill-height d-flex>
+    <v-flex fill-height d-flex v-if="recordings.length && credentials.valid">
       <v-layout row wrap style="height: auto !important;">
         <v-flex xs12 sm6>
           <record-transcript></record-transcript>
@@ -14,15 +15,16 @@
         </v-flex>
       </v-layout>
     </v-flex>
-    <v-flex class="sub-footer">
+    <v-flex class="shrink" v-if="recordings.length && credentials.valid">
       <record-player></record-player>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
 import Bus from '../js/bus';
+import RecordMessages from './RecordView/RecordMessages';
 import RecordControl from './RecordView/RecordControl';
 import RecordNotifications from './RecordView/RecordNotifications';
 import RecordList from './RecordView/RecordList';
@@ -31,6 +33,7 @@ import RecordPlayer from './RecordView/RecordPlayer';
 
 export default {
   components: {
+    RecordMessages,
     RecordControl,
     RecordNotifications,
     RecordList,
@@ -40,31 +43,17 @@ export default {
   mounted() {
     this.getRecordings();
     Bus.$on('recordingSaved', this.getRecordings);
-    Bus.$on('processError', this.handleError);
+  },
+  computed: {
+    ...mapGetters({
+      recordings: 'getRecordings',
+      credentials: 'getCredentials',
+    }),
   },
   methods: {
     ...mapActions({
       getRecordings: 'getRecordings',
     }),
-    handleError() {
-
-    },
   },
 };
 </script>
-
-<style>
-.main {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-}
-
-.main
-
-.main .card {
-  width: 100%;    
-}
-</style>
---
+++ src/renderer/components/RecordView/RecordMessages.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-flex class="shrink">
+    <v-card v-if="welcome || credentialErrors">
+      <v-card-text v-if="welcome">
+        Welcome to the Transcriber! Before you can begin transcribing audio, you must input your Google Cloud Speech and Storage API information.
+      </v-card-text>
+      <v-card-text v-if="credentialErrors">
+        Credentials are misconfigured.
+      </v-card-text>
+      <v-card-actions>
+        <v-btn @click="openSettings">
+          Fix Credentials
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-flex>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  computed: {
+    ...mapGetters({
+      credentials: 'getCredentials',
+    }),
+    welcome() {
+      return this.credentials && !this.credentials.data.saved;
+    },
+    credentialErrors() {
+      return this.credentials && this.credentials.data.saved && this.credentials.errors.length;
+    },
+  },
+  methods: {
+    openSettings() {
+      this.$router.push('/settings');
+    },
+  },
+};
+</script>
